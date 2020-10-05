4c81cc93e72f934bc73320f6f369d34606ea8c78
bramos33 <bramosd@yahoo.com>
2020-10-05 16:04:33 -0400

    update queries and ERD

--- api/history/HistoryQueries.php
+++ api/history/HistoryQueries.php
@@ -1,10 +1,14 @@
 <?php
 function history($user_id) {
     global $db;
-    $query = 'SELECT * FROM History WHERE User_user_id = ? ORDER BY date ';
+    $query = 'SELECT (Course.course_name, Party.party_etime, Score.stroke, Hole.hole_number) FROM History JOIN Score JOIN Party JOIN Course JOIN Hole'
+            . 'WHERE History.Score_Hole_course_id = Course.course_id and'
+            . 'History.Party_party_id = Party.party_id and History.Score_score_id = Score.score_id and'
+            . 'History.Score_Hole_hole_id = Hole.hole_id and'
+            . 'History.user_id = ? and Party.party_etime != NULL';
     try {
         $statement = $db->prepare($query);
-        $statement->bind_param('s', $user_id);
+        $statement->bind_param('i', $user_id);
         $statement->execute();
         $result = $statement->get_result();
         $res = $result->fetch_assoc();
