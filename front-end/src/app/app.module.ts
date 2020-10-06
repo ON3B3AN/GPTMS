63ff6954050a7c43d5611f5bba2a35d568a39161
lramirez11 <43788524+lramirez11@users.noreply.github.com>
2020-10-05 23:37:18 -0400

    fix courses

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
@@ -15,7 +16,6 @@ import { TrackComponent } from './track/track.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PlayComponent } from './play/play.component';
 import { ProfileComponent } from './profile/profile.component';
-import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -32,12 +32,11 @@ import {HttpClientModule} from '@angular/common/http';
     ProfileComponent
   ],
   imports: [
-    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    HttpClientModule,
-    MatToolbarModule,
     ReactiveFormsModule,
+    AppRoutingModule,
+    MatToolbarModule,
     NgbModule
   ],
   providers: [],
