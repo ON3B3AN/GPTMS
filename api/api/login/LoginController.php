6a3b88cf0e5ddda8b48880332463b4763a598e95
Travis <onebean91@yahoo.com>
2020-10-05 22:36:42 -0400

    Trying to learn Git

---
+++ api/api/login/LoginController.php
@@ -0,0 +1,82 @@
+<?php
+require('../db/databaseConnect.php');
+require('../login/LoginQueries.php');
+
+/*********************************************
+ * Get request from user
+ **********************************************/
+
+// Request and parse the URL to find Collection and Service
+$url = explode('/', trim($_SERVER['REQUEST_URI'],'/'));
+$collection = $url[2];
+echo "Collection: ".$collection."\n";
+
+// Check if URL includes a Collection, Service, or Service Parameters
+if (count($url) == 4) {
+    $service = $url[3];
+    $pos = strpos($service, "?=");
+    if ($pos == TRUE) {
+        $service_url = explode("?=", $service);
+        $service = $service_url[0];
+        echo "Service: ".$service."\n";
+        $param = $service_url[1];
+        echo "Service Parameter: ".$param."\n\n";
+    }
+    elseif ($pos == FALSE) {
+        echo "Service: ".$service."\n\n";
+    }
+}
+elseif (count($url) == 3) {
+    $service = NULL;
+}
+
+// Check input for HTTP method POST and JSON decode it
+$input = json_decode(file_get_contents("php://input"));
+$data = strtolower(filter_input(INPUT_POST, 'data'));
+$data = $input->data;
+
+// Check input for HTTP method GET and JSON decode it.
+if ($data == NULL) {
+    $data = strtolower(filter_input(INPUT_GET, 'data'));
+    $data = $input->data;
+    // Set error case if input POST/GET data is NULL
+    if ($data == NULL) {
+        $service = 'error';
+    }
+}
+
+/**********************************************
+ * Build and execute requested query
+ **********************************************/
+
+switch ($service) {
+    case 'error':
+        header('Access-Control-Allow-Headers: Access-Control-Allow-Origin');
+        header('Access-Control-Allow-Origin: *');
+        http_response_code(501);
+        echo http_response_code().": Error, service not recognized";
+        break;
+    default:
+        // get row
+        $email = $input->data->email;
+        $pwd = $input->data->password;
+
+        // Get result from SQL query
+        $result = login($email, $pwd);
+
+        if ($result != NULL) {
+            header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type');
+            header('Access-Control-Allow-Origin: *');
+            header('Content-Type: application/json, charset=utf-8');
+            echo json_encode($result);
+        } 
+        else {
+            header('Access-Control-Allow-Headers: Access-Control-Allow-Origin');
+            header('Access-Control-Allow-Origin: *');
+            header('Accept: application/json, charset=utf-8');
+            header('WWW-Authenticate: Basic; realm="Access to the landing page"');
+            http_response_code(401);
+            echo http_response_code().": Login failed";
+        }
+        break;
+}
