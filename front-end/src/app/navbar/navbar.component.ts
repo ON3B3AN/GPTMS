251061d81d66f535d4bdda9fdd329859dc6d24c8
lramirez11 <43788524+lramirez11@users.noreply.github.com>
2020-10-05 22:57:12 -0400

    Fix Navbar when screen size shrinks

--- front-end/src/app/navbar/navbar.component.ts
+++ front-end/src/app/navbar/navbar.component.ts
@@ -2,12 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.sass']
 })
 export class NavbarComponent implements OnInit {
+  navbarOpen = false;
+  isOpen = false;
+
+  // tslint:disable-next-line: typedef
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+
 
   constructor(private authService: AuthService, private router: Router) { }
 
