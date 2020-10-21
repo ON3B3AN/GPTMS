a8d2049e4fdad5f02dfb2da9b5dac1d8482317c8
Quentin <qsheets095@gmail.com>
2020-10-21 02:44:25 -0400

    Add a map to course detail page
    Tracking page coming soon

--- front-end/src/app/course-detail/course-detail.component.ts
+++ front-end/src/app/course-detail/course-detail.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import * as L from 'leaflet';
+
 import {CourseService} from '../course.service';
 import {Course} from '../course';
 
@@ -8,9 +10,10 @@ import {Course} from '../course';
   templateUrl: './course-detail.component.html',
   styleUrls: ['./course-detail.component.sass']
 })
-export class CourseDetailComponent implements OnInit {
+export class CourseDetailComponent implements OnInit, AfterViewInit {
   id: number;
   course: Course;
+  private map;
 
   constructor(private courseService: CourseService, private route: ActivatedRoute) { }
 
@@ -22,4 +25,20 @@ export class CourseDetailComponent implements OnInit {
       .subscribe(data => this.course = data);
   }
 
+  ngAfterViewInit(): void {
+    this.initMap();
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 42.331429, -83.045753 ],
+      zoom: 12
+    });
+  }
 }
