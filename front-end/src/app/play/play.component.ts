3be75937e9aa4d533f3b4d39f9e63329c7c9fdf8
lramirez11 <43788524+lramirez11@users.noreply.github.com>
2020-10-28 23:50:35 -0400

    Redo last commit

--- front-end/src/app/play/play.component.ts
+++ front-end/src/app/play/play.component.ts
@@ -1,8 +1,6 @@
-import { Hole } from './../hole';
 import { Component, OnInit } from '@angular/core';
-import {Course} from '../course';
-import {CourseService} from '../course.service';
-
+import {Course} from "../course";
+import {CourseService} from "../course.service";
 
 @Component({
   selector: 'app-play',
@@ -10,25 +8,13 @@ import {CourseService} from '../course.service';
   styleUrls: ['./play.component.sass']
 })
 export class PlayComponent implements OnInit {
-  holes: Hole[];
+
   courses: Course[];
-
-
-  courseChange(evt){
-    console.log(evt.target.value,this.courses)
-    this.courses.filter( element =>{
-      if(element.hole == evt.target.value){
-        this.holes = element.hole;
-      }
-    })
-  }
+  play_tee: string
   constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
     this.courseService.getCourses()
       .subscribe(data => this.courses = data);
-
   }
-
-
 }
