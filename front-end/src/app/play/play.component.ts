b5cf525a6076324a49e1420d15ca5132cfa17e16
luis ramirez <lramirez@oakland.edu>
2020-11-09 19:49:35 -0500

    Play form, navbar edit

--- front-end/src/app/play/play.component.ts
+++ front-end/src/app/play/play.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Course} from "../course";
 import {CourseService} from "../course.service";
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-play',
@@ -8,17 +11,40 @@ import {CourseService} from "../course.service";
   styleUrls: ['./play.component.sass']
 })
 export class PlayComponent implements OnInit {
+  playForm: FormGroup;
+  isSubmitted  =  false;
 
   courses: Course[];
-  play_tee: string
-  constructor(private courseService: CourseService) { }
+  tees = ['Blue', 'Gold', 'Black', 'White'];
+  rounds = ['Front 9', 'Back 9', 'Whole 18'];
+
+
+  constructor(private courseService: CourseService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.playForm  =  this.formBuilder.group({
+      courseName: ['', [Validators.required]],
+    });
     this.courseService.getCourses()
       .subscribe(data => this.courses = data);
   }
-  courseChange(e){
-    this.courses = e.target.data
-}
-}
+  get formControls() { return this.playForm.controls; }
+
+  populateTees(e) {
+    const selectedCourse = e.target.value;
+    console.log(selectedCourse)
+  }
+  onSubmit() {
+    this.isSubmitted = true;
+    if (this.playForm.invalid){
+      return;
+    }
+    else {
+      alert(JSON.stringify(this.playForm.value))
+    }
+
+  }
+  }
+
+
 
