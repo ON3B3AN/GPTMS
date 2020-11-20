9e61643c926e03e202b11c2d8d57dfbe4802cee4
cmjunkersfeld <60198809+cmjunkersfeld@users.noreply.github.com>
2020-11-19 22:10:04 -0500

    created employee management controller and queries
    crud operations

---
+++ api/employee-management/EmployeeMngmtController.php
@@ -0,0 +1,423 @@
+<?php
+/***********************
+ * Cross-Origin Policy
+************************/
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");         
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
+require('../database-management/databaseConnect.php');
+require('./EmployeeMngmtQueries.php');
+
+
+/*****************************************
+ * Declare and Initialize Local Variables
+*****************************************/
+
+$function = NULL;
+$url = NULL;
+$document = NULL;
+$collection = NULL;
+$collectionURI = NULL;
+$storeURI = NULL;
+$store = NULL;
+$controller = NULL;
+$filter = NULL;
+$filterVal = NULL;
+
+
+/*************
+ * Get URL
+*************/
+
+// Get and parse the server URL
+$url = explode('/', trim(filter_input(INPUT_SERVER, 'REQUEST_URI'),'/'));
+
+
+/**************************************************************************************
+ * -------- URI Case Templates --------
+ * Case 4 -> http://localhost/GPTMS/api/document/collection
+ * Case 4 -> http://localhost/GPTMS/api/document/collection?filter={filterVal}
+ * Case 5 -> http://localhost/GPTMS/api/document/collection/{collectionURI}
+ * Case 5 -> http://localhost/GPTMS/api/document/collection/controller
+ * Case 6 -> http://localhost/GPTMS/api/document/collection/{collectionURI}/store
+ * Case 7 -> http://localhost/GPTMS/api/document/collection/{collectionURI}/store/{storeURI}
+ * Case 7 -> http://localhost/GPTMS/api/document/collection/{collectionURI}/store/controller
+ * -------- URI Resource Indexing --------
+ * URL length [3] -> Document
+ * URL length [4] -> Collection w/ OR w/o Query Component
+ * URL length [5] -> Collection URI OR Controller
+ * URL length [6] -> Store
+ * URL length [7] -> Store URI OR Controller
+ * -------- Resource Options --------
+ * Document => employee-management
+ * Collection => employees
+ * Collection URI => emp_id
+ * Store => N/A
+ * Store URI => N/A
+ * Controller => N/A
+***************************************************************************************/
+
+// Switch cases based on URI length
+switch (count($url)) {
+    case 4:
+        $document = $url[2];
+        $collection = $url[3];
+        
+        // Check Collecion for a Query Component
+        $pos = strpos($collection, "?");
+        if ($pos == TRUE) {
+            $collectionExploded = explode("?", $collection);
+            $collection = $collectionExploded[0];
+            $filter = $collectionExploded[1];
+            
+            // Check the Filter for a Value(s)
+            $pos = strpos($filter, "=");
+            if ($pos == TRUE) {
+                $filterExploded = explode("=", $filter);
+                $filter = $filterExploded[0];
+                $filterVal = $filterExploded[1];
+            }
+        }
+        break;
+    case 5:
+        $document = $url[2];
+        $collection = $url[3];
+        
+        // Check Collecion for a Query Component
+        $pos = strpos($collection, "?");
+        if ($pos == TRUE) {
+            $collectionExploded = explode("?", $collection);
+            $collection = $collectionExploded[0];
+            $filter = $collectionExploded[1];
+            
+            // Check the Filter for a Value(s)
+            $pos = strpos($filter, "=");
+            if ($pos == TRUE) {
+                $filterExploded = explode("=", $filter);
+                $filter = $filterExploded[0];
+                $filterVal = $filterExploded[1];
+            }
+        }
+        
+        // Check if URL index [4] is a controller name or URI numerical value
+        if(is_numeric($url[4])) {
+            $collectionURI = $url[4];
+        }
+        else {
+            $controller = $url[4];
+        }     
+        break;
+    case 6:
+        $document = $url[2];
+        $collection = $url[3];
+        
+        // Check Collecion for a Query Component
+        $pos = strpos($collection, "?");
+        if ($pos == TRUE) {
+            $collectionExploded = explode("?", $collection);
+            $collection = $collectionExploded[0];
+            $filter = $collectionExploded[1];
+            
+            // Check the Filter for a Value(s)
+            $pos = strpos($filter, "=");
+            if ($pos == TRUE) {
+                $filterExploded = explode("=", $filter);
+                $filter = $filterExploded[0];
+                $filterVal = $filterExploded[1];
+            }
+        }
+        else {
+            $collectionURI = $url[4];
+            $store = $url[5];
+        }
+        break;
+    case 7:
+        $document = $url[2];
+        $collection = $url[3];
+        
+        // Check Collecion for a Query Component
+        $pos = strpos($collection, "?");
+        if ($pos == TRUE) {
+            $collectionExploded = explode("?", $collection);
+            $collection = $collectionExploded[0];
+            $filter = $collectionExploded[1];
+            
+            // Check the Query for a Value(s)
+            $pos = strpos($filter, "=");
+            if ($pos == TRUE) {
+                $filterExploded = explode("=", $filter);
+                $filter = $filterExploded[0];
+                $filterVal = $filterExploded[1];
+            }
+        }
+        else {
+            $collectionURI = $url[4];
+            $store = $url[5];
+            
+            // Check if URL index [6] is a controller name or URI numerical value
+            if(is_numeric($url[6])) {
+                $storeURI = $url[6];
+            }
+            else {
+                $controller = $url[6];
+            }
+        }
+        break;
+    default:
+        $function = "error";
+        break;
+}
+
+
+/********************************
+ * Get request & data from user
+*********************************/
+
+// Prepare input
+$input = json_decode(file_get_contents("php://input"));
+
+// Check input GET/POST/SERVER for data, if data exists then decode it
+$data = strtolower(filter_input(INPUT_POST, 'data'));
+if ($data != NULL) {
+    $data = $input->data;
+}
+elseif ($data == NULL) {
+    $data = strtolower(filter_input(INPUT_GET, 'data'));
+    if ($data != NULL) {
+        $data = $input->data;
+    }
+    elseif ($data == NULL) {
+        $data = strtolower(filter_input(INPUT_SERVER, 'data'));
+        if ($data != NULL) {
+            $data = $input->data;
+        }
+    }
+}
+
+
+/*******************************************************************
+ * Validate and assign a function(s) to each GET/POST/DELETE/PUT
+********************************************************************/
+
+if (empty($input->data)) {
+    $exists = FALSE;
+}
+else {
+    $exists = TRUE;
+}
+
+if ($exists == TRUE && $_SERVER['REQUEST_METHOD'] == "POST") {
+    // GPTMS/api/employee-management/employees
+    if ($document == "employee-management" && $collection == "employees" && $controller == NULL && $collectionURI == NULL && $filter == NULL && $filterVal == NULL && $store == NULL && $storeURI == NULL) {
+        $function = "insertEmployee";
+    }
+    else {
+        $function = "error";
+    }
+}
+elseif ($exists == FALSE && $_SERVER['REQUEST_METHOD'] == "GET") {
+    // GPTMS/api/employee-management/employees
+    if ($document == "employee-management" && $collection == "employees" && $controller == NULL && $collectionURI == NULL && $filter == NULL && $filterVal == NULL && $store == NULL && $storeURI == NULL) {
+        $function = "selectAllEmployees";
+    }
+    // GPTMS/api/employee-management/employees/1
+    elseif ($document == "employee-management" && $collection == "employees" && $controller == NULL && $collectionURI != NULL && $filter == NULL && $filterVal == NULL && $store == NULL && $storeURI == NULL) {
+        $function = "selectEmployee";
+    }
+    // GPTMS/api/employee-management/employees?course_id=1
+    elseif ($document == "employee-management" && $collection == "employees" && $controller == NULL && $collectionURI == NULL && $filter == "course_id" && $filterVal != NULL && $store == NULL && $storeURI == NULL) {
+        $function = "selectEmployeeByCourse";
+    }
+    else {
+        $function = "error";
+    }
+}
+elseif ($exists == TRUE && $_SERVER['REQUEST_METHOD'] == "PUT") {
+    // GPTMS/api/employee-management/employees/1
+    if ($document == "employee-management" && $collection == "employees" && $controller == NULL && $collectionURI != NULL && $filter == NULL && $filterVal == NULL && $store == NULL && $storeURI == NULL) {
+        $function = "updateEmployee";
+    }
+    else {
+        $function = "error";
+    }
+}
+elseif ($exists == FALSE && $_SERVER['REQUEST_METHOD'] == "DELETE") {
+   // GPTMS/api/employee-management/employees/1
+    if ($document == "employee-management" && $collection == "employees" && $controller == NULL && $collectionURI != NULL && $filter == NULL && $filterVal == NULL && $store == NULL && $storeURI == NULL) {
+        $function = "deleteEmployee";
+    }
+    else {
+        $function = "error";
+    }
+}
+else {
+    $function = "error";
+}
+
+
+/**************************************************************
+ * Build and execute requested controller function &  SQL query
+**************************************************************/
+
+switch ($function) {
+    case "error":
+        header('Accept: application/json, charset=utf-8');
+        http_response_code(501);
+        echo http_response_code().": Error, service not recognized";
+        break;
+    case "insertEmployee":
+        // Get JSON data
+        $user_id = $input->data->User_user_id;
+        $course_id = $input->data->Course_course_id;
+        $security_lvl = $input->data->security_lvl;
+        
+        // Get result from SQL query
+        $result = insertEmployee($user_id, $course_id, $security_lvl);
+        
+        if ($result != NULL) {
+            http_response_code(201);
+            echo http_response_code().": Employee created successfully";
+        } 
+        else {
+            header('Accept: application/json');
+            http_response_code(500);
+            echo http_response_code().": Error, employee not created";
+        }
+        break;
+    case "updateEmployee":
+        // Get JSON data
+        $course_id = $input->data->Course_course_id;
+        $security_lvl = $input->data->security_lvl;
+        
+        // Assign collection URI to emp_id
+        $emp_id = $collectionURI;
+
+        // Get the updated row count from the SQL query
+        $result = updateEmployee($course_id, $security_lvl, $emp_id);
+
+        if ($result >= 1) {
+            http_response_code(200);
+            echo http_response_code().": Employee updated successfully";
+        }
+        // No changes were made (Acts as a "Save" function)
+        elseif ($result === 0) {
+            http_response_code(204);
+            echo http_response_code();
+        }
+        else {
+            header('Accept: application/json');
+            http_response_code(404);
+            echo http_response_code().": Error, employee not updated";
+        }
+        
+        break;
+    case "deleteEmployee":
+        // Assign collection URI to emp_id
+        $emp_id = $collectionURI;
+
+        // Get number of rows affected from SQL query
+        $result = deleteEmployee($emp_id);
+        
+        if ($result >= 1) {
+            http_response_code(200);
+            echo http_response_code().": Employee deleted successfully";
+        }
+        else {
+            http_response_code(404);
+            echo http_response_code().": Error, employee not deleted";
+        }
+        break;
+    case "selectAllEmployees":
+        // Get result from SQL query
+        $result = selectAllEmployees();
+
+        if ($result != NULL) {
+            header('Content-Type: application/json, charset=utf-8');
+            http_response_code(200);
+
+            // Return user data as JSON array
+            echo json_encode($result);
+        } 
+        else {
+            http_response_code(404);
+            echo http_response_code().": No employees found";
+        }
+        break;
+    case "selectEmployeeByCourse":
+        // Check if filter = course id
+        if ($filter == "course_id") {
+            //Get course id from URL
+            $course_id = $filterVal;
+        }
+        
+        // Get result from SQL query
+        $result = selectEmployeeByCourse($course_id);
+
+        if ($result != NULL) {
+            header('Content-Type: application/json, charset=utf-8');
+            http_response_code(200);
+
+            // Return user data as JSON array
+            echo json_encode($result);
+        } 
+        else {
+            http_response_code(404);
+            echo http_response_code().": No employees found";
+        }
+        break;
+    case "selectEmployee":
+        // Get employee id from URL
+        $emp_id = $collectionURI;
+        
+        // Get result from SQL query
+        $result = selectEmployee(emp_id);
+        
+        if ($result != NULL) {
+            header('Content-Type: application/json, charset=utf-8');
+            http_response_code(200);
+
+            // Return employee data as JSON array
+            echo json_encode($result);
+        } 
+        else {
+            http_response_code(404);
+            echo http_response_code().": No employees found";
+        }
+        break;
+}
+
+
+/********************
+ * Troubleshooting
+*********************/
+
+//echo "\n\n"."URL ";
+//print_r($url);
+//echo "\n"."HTTP Method: ".$_SERVER['REQUEST_METHOD'];
+//echo "\n"."URL count: ".count($url)."\n";
+//echo "Data exists (1=TRUE,''=FALSE): ".$exists."\n";
+//if ($exists == "TRUE") {
+//    echo "Data: "."\n";
+//    print_r($input->data);
+//}
+//echo "Document: ".$document."\n";
+//echo "Collection: ".$collection."\n";
+//echo "Collection URI: ".$collectionURI."\n";
+//echo "Store: ".$store."\n";
+//echo "Store URI: ".$storeURI."\n";
+//echo "Controller: ".$controller."\n";
---
+++ api/employee-management/README.md
@@ -0,0 +1,88 @@
+# Employee Management: Routes/Endpoints
+
+## [SELECT ALL EMPLOYEES]
+### URI [GET] : http://localhost/employee-management/employees
+* This URI requests the "employee-management" document and “employees” collection. This request must not include any data.
+#### Example Input: N/A
+#### Output: Successful
+    * Header: 200: Ok
+    * Body (app/json): Returns all employees
+#### Output: Unsuccessful
+    * Header: 404: Page Not Found
+    * Body (text/html): 404: No employees found
+#### Output: Server Error
+    * Header: 501: Not Implemented
+    * Body (text/html): 501: Error, service not recognized
+
+## [SELECT EMPLOYEE]
+### URI [GET] : http://localhost/employee-management/employees/1
+* This URI requests the "employee-management" document, “employees” collection, and "1" collection URI (represents employee id). This request must not include any data.
+#### Example Input: N/A
+#### Output: Successful
+    * Header: 200: Ok
+    * Body (app/json): Returns employee
+#### Output: Unsuccessful
+    * Header: 404: Page Not Found
+    * Body (text/html): 404: No employee with id=1 found
+#### Output: Server Error
+    * Header: 501: Not Implemented
+    * Body (text/html): 501: Error, service not recognized
+
+## [INSERT EMPLOYEE]
+### URI [POST] : http://localhost/employee-management/employees
+* This URI requests the "employee-management" document and “employee” collection. This request must include relevant data.
+#### Example Input: {"data":{"User_user_id":"3","Course_course_id":"1","security_lvl":"0"}}
+#### Output: Successful
+    * Header: 201: Created
+    * Body (text/html): 201: Employee added successfully
+#### Output: Unsuccessful
+    * Header: 404: Page Not Found
+    * Body (text/html): 404: Error, employee not added
+#### Output: Server Error
+    * Header: 501: Not Implemented
+    * Body (text/html): 501: Error, service not recognized
+
+## [UPDATE EMPLOYEE]
+### URI [PUT] : http://localhost/employee-management/employees/1
+* This URI requests the "employee-management" document, “employees” collection, and "1" collection URI (represents employee id). This request must include relevant data.
+#### Example Input: {"data":{"Course_course_id":"1","security_lvl":"0"}}
+#### Output: Successful
+    * Header: 200: Ok
+    * Body (text/html): 200: Employee updated successfully
+#### Output: Successful (no changes were made)
+    * Header: 204: No content
+    * Body: N/A
+#### Output: Unsuccessful
+    * Header: 404: Page Not Found
+    * Body (text/html): 404: Error, employee not updated
+#### Output: Server Error
+    * Header: 501: Not Implemented
+    * Body (text/html): 501: Error, service not recognized
+
+## [DELETE EMPLOYEE]
+### URI [DELETE] : http://localhost/employee-management/employee/1
+* This URI requests the "employee-management" document, “employees” collection, and "1" collection URI (represents employee id). This request must not include any data.
+#### Example Input: N/A
+#### Output: Successful
+    * Header: 200: Ok
+    * Body (text/html): 200: Employee deleted successfully
+#### Output: Unsuccessful
+    * Header: 404: Page Not Found
+    * Body (text/html): 404: Error, employee not deleted
+#### Output: Server Error
+    * Header: 501: Not Implemented
+    * Body (text/html): 501: Error, service not recognized
+
+## [SELECT EMPLOYEE BY COURSE]
+### URI [GET] : http://localhost/employee-management/employees?course_id=1
+* This URI requests the "employee-management" document, “employees” collection, "course_id" filter, and "1" filterVal. This request must not include any data.
+#### Example Input: N/A
+#### Output: Successful
+    * Header: 200: Ok
+    * Body (app/json): Returns all employees by course id
+#### Output: Unsuccessful
+    * Header: 404: Page Not Found
+    * Body (text/html): 404: Error, no employees found
+#### Output: Server Error
+    * Header: 501: Not Implemented
+    * Body (text/html): 501: Error, service not recognized
