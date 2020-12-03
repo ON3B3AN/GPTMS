3a79723656d7a185af9a761a825c9224ce111133
bramos33 <bramosd@yahoo.com>
2020-12-02 21:32:59 -0500

    Update CourseMngmtController.php

--- api/course-management/CourseMngmtController.php
+++ api/course-management/CourseMngmtController.php
@@ -2,6 +2,7 @@
 /***********************
  * Cross-Origin Policy
 ************************/
+error_reporting(0);
 
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
