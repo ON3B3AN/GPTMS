43ba9b8e0d910d3a268cf1727d8da082c365db02
jwwelchner <44379535+jwwelchner@users.noreply.github.com>
2020-12-12 18:40:46 -0500

    footer, other stuff
    Co-Authored-By: Quentin <1476108+quentinxs@users.noreply.github.com>

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -34,10 +34,12 @@ import { GameComponent } from './game/game.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardComponent } from './landing/card/card.component';
 import { CourseOverviewComponent } from './course-detail/course-overview/course-overview.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
     LoginComponent,
     SignupComponent,
     LandingComponent,
